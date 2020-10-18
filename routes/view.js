e4983deec1838c0a094be50da5b8c2d2da4cc6ee
Reed Santos <reedsantos@icloud.com>
2020-10-18 17:45:45 -0400

    add comments

--- routes/view.js
+++ routes/view.js
@@ -1,10 +1,13 @@
+// dependencies
 const router = require("express").Router();
 const path = require("path");
 
+// get route for exercise page
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
+// get route for stats page
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname,"../public/stats.html"))
 });
